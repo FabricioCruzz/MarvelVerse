baad0a495d5c704c1ea6b75b83edcdc7226cb589
FabricioCruzz <fabriciocruzz77@gmail.com>
2022-05-30 00:38:54 -0300

    Heroku: Configurando arquivo 'server.js' para subir para Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://gateway.marvel.com/v1/public/',
+  publicKey: 'b2a9da6b64c0ad9cf0e7552a315a30ad',
+  hash: '9a7e1cab451a2967ffb2548b6ffe5838',
 };
